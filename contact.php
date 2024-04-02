da49a8bc75266c5ce57b2244b124d49478d7e2d5
denisstuja <denisstuja13@gmail.com>
2024-04-02 22:35:19 +0200

    .

--- contact.php
+++ contact.php
@@ -49,15 +49,6 @@
                 <label for="name"></label>
                 <input type="text" placeholder="My number is" name="telephone" id="telephone_input" required>
               </div>
-              <div class="subject">
-                <label for="subject"></label>
-                <select placeholder="Subject line" name="subject" id="subject_input" required>
-                  <option disabled hidden selected>Subject line</option>
-                  <option>I'd like to make a reservation</option>
-                  <option>I'd like to ask a question</option>
-                  <option>I'd like to make a proposals</option>
-                </select>
-              </div>
               <div class="message">
                 <label for="message"></label>
                 <textarea name="message" placeholder="I'd like to chat about" id="message_input" cols="30" rows="5" required></textarea>
